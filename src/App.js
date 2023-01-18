1e8e6669e799a5a6e187e87c2b0fb6190498f558
DjRaim <45205678+DjRaim@users.noreply.github.com>
2023-01-18 16:31:32 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Text from "./components/Text"
+import '/style.css'
+import Content from './components/Content'
+import Footer from './components/Footer'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
