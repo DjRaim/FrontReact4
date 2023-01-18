9d71970c9c2f2cd6278adee1da0a4c1798f68b2d
DjRaim <45205678+DjRaim@users.noreply.github.com>
2023-01-18 16:29:26 +0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Text from "./components/Text"
+import '/style.css'
+import Content from './components/Content'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
